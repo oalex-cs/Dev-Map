6256cf437e193dfdbdc62061782d4854529420c1
oalex-cs <alex.castilhoprogrammer@gmail.com>
2025-08-15 23:56:45 -0300

    Commit dia 15/08

--- Fundamentos.js
+++ Fundamentos.js
@@ -117,3 +117,191 @@ do{ // Dá uma oportunidade de digitar sua senha, antes de fazer a verificação
 } while(tentativa != senhaCorreta); // Em quanto a senha permanecer incorreta, ele executa o trecho acima
 
 console.log("Acesso liberado!") // Caso a senha esteja correta, libera o acesso
+
+/*For/of: Percorre diretamente os valores de uma coleçao iteravel.
+  - Iteravel: Algo que pode ser percorrido elemento por elemento (arrays, strings, map, set...) */
+
+  let frutas = ["Maça, Banana, Uva, Melancia"];
+
+  for(let fruta of frutas){
+   console.log(fruta)
+  }
+
+  /*Logica:
+   - Pega o objeto iteravel(Frutas) 
+   - Cria um iterador (Fruta) que vai pegar sempre o proximo valor
+   - Executa o trecho uma vez para cada valor
+   - Encerra quando a lista acabar, sem precisar determinar um tamanho (length) */
+
+  /*Diferença entre For(tradicional) e o For/of:
+   - No for(trad) o indice e controlado manualmente, no for/of ele e automatico
+   - No for(trad) o tamanho precisa ser determinado previamente, no for/of ele e pego automaticamente */
+
+  /*Uso recomendado:
+   - Quando o indice nao importar
+   - Deixar o codigo mais limpo e legivel
+   - Quando trabalha com coleçoes mais modernas (map, set) */
+
+  /*O for/of pega um elemento de cada vez, direto do valor, e para sozinho no final. 
+    Usar quando somento os valores forem necessarios, nao indices*/
+
+// Funçoes
+
+/*Funçao e um bloco de codigo reutilizavel que executa uma tarefa especifica. Pode ser chamado em outras partes do codigo
+sem precisar reescrever o codigo repetidas vezes */
+
+function nomeFuncao (parametro1, parametro2){ 
+   // codigo a ser executado
+   return resultado; // opcional
+}
+
+/*Por que VOCE deve usar funçoes:
+ - Evita a repetiçao do mesmo codigo
+ - Deixa o codigo mais limpo
+ - Facilita na manutençao
+ - Torna o codigo mais legivel */
+
+function somar(a, b){ // a e b = parametro 1 e 2
+   return a + b; // retorna o resultado para a chamada
+}
+
+console.log(somar(3, 4)); // output: 7
+
+// Tipos de funcoes:
+
+/*Funçao declarada: A mais tradicional, pode ser chamada ANTES de ser declarada */
+
+console.log(nameFunction(Alex)); // chamada da funçao
+
+function saudacao(nome) { // funçao declarada
+   return `Bem vindo, ${nome}`;
+}
+
+/*Funçao anonima: E armazenada em uma variavel, precisa ser declarada ANTES de ser chamada */
+
+const divisao = function(c, d) { // funçao declarada dentro de uma variavel (const)
+   return c / d;
+}
+
+console.log(divisao(10, 2)); // chamada da funçao
+
+/*Funçao sem retorno: Somente exucuta algo, sem dar um retorno(return) */
+
+function menos(e, f){
+   console.log("O resultado e: ", e - f); // sem retorno, so execuçao do codigo
+}
+
+menos(5, 2); // output: 3
+
+// callbacks: E uma funçao passada como argumento para outra funçao que e executada em um momento posterior 
+
+
+// Manipulaçao do DOM(Document Object Model)
+
+/*Ele e uma representa seu codigo HTML baseado em uma grande arvore de obejtos, onde cada elemento(tags, textos, atributos) e
+tratado com um objeto em que o Javascript consegue acessar e manipular */
+
+/*O navegador pega o HTML, transforma ele em uma grande arvore com objetos e o Javascript consegue acessar e 
+mudar essa arvore */
+
+Document
+ └── html
+      ├── head
+      │     └── title ("Meu Site")
+      └── body
+            ├── h1 ("Olá Mundo")
+            └── p ("Seja bem-vindo!")
+
+/*O DOM e importante:
+  - E a unica forma do JS ler e manipular o HTML
+  - Permite alterar textos, estilos e imagens/ Adicionar ou remover elementos
+    Responder a interaçoes do usuario
+  - Sem o DOM, JS nao poderia manipular elementos Web */
+
+// Selecionando Elementos do DOM:
+
+document.getElementById() // Seleciona elementos pelo ID
+
+document.getElementsByClassName() // Pega todos os elementos de mesma classe
+
+document.getElementsByTagName() // Pega todos os elementos pela tag
+
+// Forma moderna com seletor CSS
+document.querySelector() // Primeiro que encontrar com esse parametro
+document.querySelectorAll() // Seleciona todos os elementos com esse parametro
+
+
+// Eventos
+
+/*Eventos sao açoes que acontecem no navegador que o JS pode detectar e responder 
+
+   Exemplos de eventos:
+     - Clique em um botao (Click)
+     - Digitar em um campo (Input)
+     - Enviar um formulario (Form)
+     - Carregar uma pagina (load)
+     - Passar o mouse sobre um elemento */
+
+// Algo acontece = Codigo executado
+
+// Principais Eventos:
+/* - click: Usuario clica em algum elemento / enviar form, trocar cor
+   - input: Digita algo / Validaçao em tempo real 
+   - change: mudar valor do elemento / Seleçao de opçoes
+   - submit: enviar um formulario / Tratar dados
+   - mouseover: Mouse entra na area do elemento / adicionar destaque visual
+   - mouseout: Sai da area do elemento / remover destaque
+   - keydown: Tecla pressionada / atalhos do teclado
+   - load: Pagina ou recurso carregado / iniciar scripts apos carregar */
+
+// Como adicionar eventos a elementos: addEventListener.evento
+
+const btn = document.getElementById("Id"); // Variavel "btn" armazenando a elemento selecionado (getElementById)
+
+btn.addEventListener("click", () => { // adicionando um evento (click) ao pressionar o botao
+   alert("Voce clicou no botao!") // cria um alerta na pagina quando o botao for pressionado
+});
+
+
+// Evitando comportamentos padrao
+
+/*Alguns eventos tem um comportamento padrao definido pelo navegador e para evitar isso usamos o event/event.preventDefault() */
+
+// Por padrao, ao enviar um formulario o navegador recarrega a pagina
+document.getElementById("Form").addEventListener("submit", (event) => {
+   event.preventDefault() // impede o reload da pagina
+   alert("Enviou o formulario!")
+})
+
+// Trabalhando com o Event(Objeto)
+
+/*Toda funçao de evento recebe um objeto de evento contendo as informaçoes do que aconteceu */
+
+event.target // elemento que disparou o evento
+event.type // tipo de evento
+event.key // tecla pressionada, usada em eventos com o teclado
+
+// Adendos:
+
+/*innerHtml vs textContent:
+  - Explicando de maneira rapida e superficial cada um, innerHtml e usado para manipular elementos em HTML. Seja pra adicionar, alterar
+  ou apenas ler o elemento html. Nao e recomendado o uso, pois e lento e contem riscos de segurança dependendo da origem dos dados.
+  Ja o textContent consegue ler e manipular apenas elementos de textos, ignorando completamente qualquer tag em HTML. Mas e mais rapido
+  e seguro de utilizar.
+  
+  -Use o innerHtml apenas se precisar alterar a estrutura do HTML ou ter um HTML dinamico, se nao for o caso, de preferencia para o 
+  textContent.*/
+
+// innerHtml estrutura:
+
+const elemento = document.getElementById('meuElemento');
+
+// Obtendo o conteúdo HTML
+const conteudoAtual = elemento.innerHTML;
+console.log(conteudoAtual);
+// Output: <p>Este é um parágrafo.</p><a href="#">Clique aqui</a>
+
+// Definindo um novo conteúdo HTML
+elemento.innerHTML = '<p>Novo conteúdo!</p>';
+console.log(elemento.innerHTML);
+// Output: <p>Novo conteúdo!</p>
---
+++ lista-de-chamada.js
@@ -0,0 +1,5 @@
+const alunos = ["Ana", "Bruno", "Carla", "Diego"];
+
+for(let aluno of alunos) {
+    console.log("Aluno: ", aluno);
+}
